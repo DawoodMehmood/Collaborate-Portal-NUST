6d0cdd71f1b274d5e6f02ca9a6028e27fe7c2157
KhawajaTanzeelUrRehman <khawajatanzeel.55221@gmail.com>
2023-07-17 16:12:44 +0500

    clearing middle file

---
+++ ric-first-templat-website/src/APIs/hardcode.js
@@ -0,0 +1,125 @@
+    // Nanomaterials
+    const labsData1 = [
+        {
+            id: 1, title: 'Scanning Electron Microscopy', description: 'SEM produces largely Magnified image by using electron as source It gives the morphology of the samples at highly magnified images It also gives elemental composition of any sample (EDX)',
+            model: 'JSM-6490A', company: 'JEOL', country: 'Japan',
+            capacity: '',
+            range: '',
+            venue: 'School of Chemical and Materials Engineering (SCME)'
+        },
+        {
+            id: 2, title: 'Laser Scattering Particle Size Distribution Analyzer', description: 'The measurement of particle size distribution is used for fundamental studies and quality control in production processes in many diverse fields such as fine ceramics, cement, pharmaceuticals, metal powders, industrial minerals and ores, explosives, solid fuels, food, drugs and beauty care emulsions, micelles, polymers, coatings and adhesives, pigments and dyes, carbon black and other mineral additives, and fillers used in the rubber, plastics and paper industries',
+            model: '', company: 'HORIBA', country: '',
+            capacity: '',
+            venue: 'School of Chemical and Materials Engineering (SCME)'
+        }
+    ];
+
+    //Microwave
+    const labsData2 = [
+        {
+            id: 1, title: 'Antenna Kit ', description: '',
+            model: 'JSM-6490A', company: 'JEOL', country: 'Japan',
+            capacity: '',
+            range: '20 Hz - 40 GHz',
+            venue: 'School of Electrical, Electronics and Computer Science (SEECS)'
+        },
+        {
+            id: 1, title: 'EMI Analyzer PXE N9048B 1 Hz - 26.5 GHz ', description: 'Spectrum Analyzer for EMI Testing',
+            model: 'PXE N9048B', company: '', country: '',
+            capacity: '',
+            range: '1 Hz - 26.5 GHz',
+            venue: 'School of Electrical, Electronics and Computer Science (SEECS)'
+        }
+    ];
+
+
+    const labsData3 = [
+        {
+            id: 1, title: 'Acoustic Camera', description: 'It is used for noise and leakage detection in many different scenarios. It enable the user to visualise different sound sources at different frequencies and source strengths. It has 128 microphones with camera in middle. It is a real time virtual microphone with digital microphones and no extra acquisition unit needed.',
+            model: '', company: 'NorSonic', country: '',
+            capacity: '',
+            range: '316 Hz - 15kHz, Operating distance 0,5 m to 200 m',
+            venue: 'School of Mechanical and Manufacturing Engineering (SMME)'
+        }
+
+        // Add more lab data as needed
+    ];
+
+    const labsData4 = [
+        {
+            id: 1, title: 'Gas Chromatography Mass Spectrometry', description: 'Measurement of qualitative and quantitative analysis of composition of different organic materials.',
+            model: '', company: 'Philips', country: '',
+            capacity: '4-7 Samples/Day',
+            range: '',
+            venue: 'School of Natural Sciences (SNS)'
+        },
+        {
+            id: 2, title: 'High Performance Liquid Chromatography (HPLC)', description: 'HPLC is an important analytical method commonly used to separate and quantify components of liquid samples',
+            model: '', company: '', country: '',
+            capacity: 'Column Heater, Binary HPLC Pump, UV/Vis Detector, 10 samples / day',
+            range: '',
+            venue: 'School of Natural Sciences'
+        }
+
+        // Add more lab data as needed
+    ];
+
+    const labsData5 = [
+        {
+            id: 1, title: 'No data found under this field.',
+            model: '', company: '', country: '',
+            capacity: '',
+            range: '',
+            venue: ''
+        }
+
+        // Add more lab data as needed
+    ];
+
+
+    const industryData1 = [
+        {
+            id: 1, title: 'Pharmatec Pakistan (Pvt) Ltd',
+            description: "One of the most dynamic and fast-growing privately-owned pharmaceutical companies in Pakistan, that is developing, manufacturing and supplying pharmaceutical generics and value-added products.",
+            services: "Injectable plant Creams, ointment and gel area, Most modern facilities for liquid/drops, tablet/capsules/caplets manufacturing & packaging area (Cardiovascular, anti-diabetic, psychiatry, pain management, anti gout, respiratory disease, gastro-intestinal, anti-biotics & vitamins medicines), State of art Quality Control / Quality Assurance laboratories, R&D team with years of experience in formulation development",
+            contact: "www.pharmatec.com"
+        },
+    ];
+
+
+    const industryData2 = [
+        {
+            id: 1, title: 'National Radio Telecom Corporation (NRTC)',
+            description: "National Radio Telecommunication Corporation the high tech industry engaged in manufacturing of telecommunication equipment in Pakistan. NRTC is the pioneer in Telecommunication Equipment in Pakistan and leader in the field of communication for the last three decades. NRTC is producing high quality ruggedized products to be used in harsh environment such as defense services, Para / Auxiliary security services. Commercial products and versions for use by civil Telecommunications operators and civil organizations / establishments since 1966.",
+            services: "LMR, JAMMERs, ANTENNAs, MICROWAVE, ENCRYPTION DEVICES, COMSEC SYSTEMS, Tracking systems, Homeland security, TETRA, Electronic Warfare, DIGITAL INTERCOM, IP INTERFACES, Solar Solution, LI systems, Smart automation, UGS, Digital Investigation suite, EOD/ROBOTS, Field Exchanges",
+            contact: "www.nrtc.com.pk"
+        },
+    ]
+
+    const industryData3 = [
+        {
+            id: 1, title: 'Turkish Aerospace',
+            description: "The centre of technology in design, development, manufacturing and integration of aerospace systems, modernisation and after sales support in Turkey.",
+            services: "Aircraft, Helicopter, UAV, Space, Component Production",
+            contact: "www.tai.com.tr"
+
+        },
+    ]
+
+    const industryData4 = [
+        {
+            id: 1, title: 'Chemonics International Inc',
+            description: "A private international development firm based in Washington DC, USA, offering a variety of services globally. With more than USD 1.5 billion in USAID contracts in 2019, it  is the largest for-profit recipient of US Government foreign aid. As of 2019, the company has approximately 5,000 employees in 100 countries.",
+            services: "Agriculture & Food security, Environment & Natural Resources, Health, Supply chain solutions, Water, energy & sustainable cities",
+            contact: "www.chemonics.com"
+        },
+    ]
+
+    const industryData5 = [
+        {
+            id: 1, title: 'No Industry found',
+
+        },
+
+    ]
