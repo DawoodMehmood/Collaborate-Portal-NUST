e3fb5dbc91c35393a46cc20b4c16b6f9d45d814e
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-01-17 15:08:41 +0500

    Added Supervisors column names for date conversion

--- llm-server/llm.py
+++ llm-server/llm.py
@@ -287,7 +287,7 @@ def stringToNumber(df):
     return df
 
 def stringToDate(df):
-    columns_to_modify = ["IP Approval Date", "Effective School Date", "Project Approval Date", "IP Licensing Date", "IP Awarding Date", "Project Completion Date", "Project Submission Date", "Print Publication Date", "Publication year"]
+    columns_to_modify = ["Supervision Start Date", "IP Approval Date", "Effective School Date", "Project Approval Date", "IP Licensing Date", "IP Awarding Date", "Project Completion Date", "Project Submission Date", "Print Publication Date", "Publication year"]
 
     # Convert specified columns to datetime
     for col in columns_to_modify:
